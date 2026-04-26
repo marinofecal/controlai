65eccd419fef905184b57c201bc25b8918bc8d19
sky <marinofecal@gmail.com>
2026-04-26 17:55:09 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compress: true,
-  
-  headers: async () => [
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
-        },
-        {
-          key: 'X-Frame-Options',
-          value: 'DENY'
-        },
-        {
-          key: 'X-Content-Type-Options',
-          value: 'nosniff'
-        }
-      ]
-    }
-  ],
 };
 
 module.exports = nextConfig;
